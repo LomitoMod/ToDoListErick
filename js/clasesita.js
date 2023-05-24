a2e8de5ffd3bbaf8fd0b3792d73624a7ee7186ff
Erick Meza <emezamake3d@gmail.com>
2023-05-23 21:37:03 -0500

    ToDoList-3 botones

---
+++ js/clasesita.js
@@ -0,0 +1,28 @@
+
+function Suma(num1,num2){
+    const resultado=num1+num2;
+    console.log("suma: ", resultado);
+}
+
+function Resta(num1,num2){
+    let resultado=num1-num2;
+    console.log("suma: ", resultado);
+}
+
+function Multi(num1,num2){
+    let resultado=num1*num2;
+    console.log("suma: ", resultado);
+}
+
+function Divi(num1,num2){
+    let resultado=num1/num2;
+    console.log("suma: ", resultado);
+}
+
+
+Suma(50,4);
+Resta(50,4);
+Multi(50,4);
+Divi(50,4);
+
+
--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 function agregar(){
     const textoinput=document.getElementById("inputpushme").value;
     const nuevalista=document.createElement("li");
+    const nuevoboton=document.createElement("button");
+    const checkboton=document.createElement("button");
     const textoNuevoElemento = document.createTextNode(textoinput);
     nuevalista.appendChild(textoNuevoElemento);
 
     const lista = document.querySelector(".Lista");
     lista.appendChild(nuevalista);
+    nuevalista.appendChild(nuevoboton);
+    nuevalista.appendChild(checkboton);
+    nuevoboton.innerHTML="Eliminar";
+    checkboton.innerHTML="Check";
     document.getElementById("inputpushme").value = "";
 }
